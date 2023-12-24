14749efb7b011ca6a83552f53df1dbcd1d83f6a7
danielkaijzer <danielkaijzer00@gmail.com>
2023-12-24 17:13:53 -0500

    Solved LC11 using O(n) linear solution

--- LC11_Container_With_Most_Water.cpp
+++ LC11_Container_With_Most_Water.cpp
@@ -7,27 +7,32 @@
 class Solution {
 public:
     int maxArea(std::vector<int>& height) {
-        // int start =0, end = height.size()-1;
 
         int max_water = 0;
+        int left = 0, right = height.size()-1;
 
-        // brute force
-        for (int i = 0; i < height.size(); ++i){
-            for (int j = i; j < height.size(); ++j){
-                // if new max area found, update max_water
-                int cur_area = std::min(height[i],height[j]) * (j-i);
-                if (max_water < cur_area){
-                    max_water = cur_area;
-                }
+        // O(n) since we only iterate through all elements once
+        while(left < right){
+            int lowest_pillar = std::min(height[left], height[right]);
+            int cur_area = lowest_pillar * (right-left);
 
+            max_water = std::max(max_water,cur_area);
+
+            // shift pointer pointing to minimum because only a greater value can increase our area from here
+            if (height[left] >= height[right]){
+                right--;
+            }
+            else{
+                left++;
             }
         }
+        
         return max_water;
     }
 };
 
 int main(){
-    std::vector<int> h{1,8,6,2,5,4,8,3,7};
+    std::vector<int> h{1,8,6,2,5,4,8,3,7}; // 49
     Solution s;
 
     std::cout << s.maxArea(h) << std::endl;
