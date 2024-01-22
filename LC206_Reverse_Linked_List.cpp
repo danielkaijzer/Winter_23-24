1735a0dc6b0a008d5c06fd3acd114153223b2929
danielkaijzer <danielkaijzer00@gmail.com>
2024-01-22 20:55:14 +0900

    Practiced Reversing Linked List

---
+++ LC206_Reverse_Linked_List.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* prev = nullptr;
+
+        while(cur != nullptr){ // if not tail
+            ListNode* tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+
+        // 1->2->3->nullptr
+        // nullptr<-1<-2<-3
+
+        // at each step along the chain, starting from head,
+        // we need to:
+        // 
+        // 
+        return prev;
+
+    }
+};
+
+int main(){
+Solution s;
+
+    // Create a linked list
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    // head->next->next->next = new ListNode(4);
+    // head->next->next->next->next = new ListNode(5);
+
+    // Reverse the linked list
+    ListNode* reversedHead = s.reverseList(head);
+
+    // Print the reversed linked list
+    while (reversedHead != nullptr){
+        std::cout << reversedHead->val << " ";
+        reversedHead = reversedHead->next;
+    }
+
+    return 0;
+}
